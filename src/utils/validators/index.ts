a3a038dc07899faa51591e44039d688359f63f04
Alex Gola <alex.gola93@gmail.com>
2025-11-03 09:52:00 +0100

    Migration completed

--- src/utils/validators/index.ts
+++
@@ -1,5 +0,0 @@
-import moment from "moment";
-
-export const fixIsNaN = value => !(!isNaN(value) && typeof value === 'number');
-export const isValidMonthIndex = monthIndex => (monthIndex >= 1 &&  monthIndex <= 12);
-export const isValidYear = year => (year >= 1990 &&  year <= (moment().year() + 100));
