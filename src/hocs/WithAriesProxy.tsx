880d9662b133900c834974dd851f9875ceae4dcb
Alex Gola <alex.gola93@gmail.com>
2025-11-03 09:51:15 +0100

    Migration completed

--- src/hocs/WithAriesProxy.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import ariesProxy from '../proxies/aries-proxy'
-
-export default (WrapperComponent) => (props) => <WrapperComponent ariesProxy={ariesProxy} {...props}/>;
--- src/styles/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './style-utils';
-export * from './containers';
-export * from './colors';
